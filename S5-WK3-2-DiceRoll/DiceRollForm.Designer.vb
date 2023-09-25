c2998b16fbc33debf91490173d3e2c47ecd6535e
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-09-24 22:11:39 -0600

    Done

--- S5-WK3-2-DiceRoll/DiceRollForm.Designer.vb
+++ S5-WK3-2-DiceRoll/DiceRollForm.Designer.vb
@@ -22,17 +22,66 @@ Partial Class DiceRollForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.GenerateButton = New System.Windows.Forms.Button()
+        Me.DisplayListBox = New System.Windows.Forms.ListBox()
+        Me.QuitButton = New System.Windows.Forms.Button()
+        Me.SelectedIndexLabel = New System.Windows.Forms.Label()
         Me.SuspendLayout()
+        '
+        'GenerateButton
+        '
+        Me.GenerateButton.Location = New System.Drawing.Point(12, 12)
+        Me.GenerateButton.Name = "GenerateButton"
+        Me.GenerateButton.Size = New System.Drawing.Size(150, 91)
+        Me.GenerateButton.TabIndex = 0
+        Me.GenerateButton.Text = "Generate"
+        Me.GenerateButton.UseVisualStyleBackColor = True
+        '
+        'DisplayListBox
+        '
+        Me.DisplayListBox.FormattingEnabled = True
+        Me.DisplayListBox.ItemHeight = 20
+        Me.DisplayListBox.Location = New System.Drawing.Point(168, 12)
+        Me.DisplayListBox.Name = "DisplayListBox"
+        Me.DisplayListBox.Size = New System.Drawing.Size(130, 344)
+        Me.DisplayListBox.TabIndex = 1
+        '
+        'QuitButton
+        '
+        Me.QuitButton.Location = New System.Drawing.Point(12, 109)
+        Me.QuitButton.Name = "QuitButton"
+        Me.QuitButton.Size = New System.Drawing.Size(150, 91)
+        Me.QuitButton.TabIndex = 2
+        Me.QuitButton.Text = "Quit"
+        Me.QuitButton.UseVisualStyleBackColor = True
+        '
+        'SelectedIndexLabel
+        '
+        Me.SelectedIndexLabel.AutoSize = True
+        Me.SelectedIndexLabel.Location = New System.Drawing.Point(12, 203)
+        Me.SelectedIndexLabel.Name = "SelectedIndexLabel"
+        Me.SelectedIndexLabel.Size = New System.Drawing.Size(141, 20)
+        Me.SelectedIndexLabel.TabIndex = 3
+        Me.SelectedIndexLabel.Text = "DisplayListBoxIndex"
         '
         'DiceRollForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(323, 373)
+        Me.Controls.Add(Me.SelectedIndexLabel)
+        Me.Controls.Add(Me.QuitButton)
+        Me.Controls.Add(Me.DisplayListBox)
+        Me.Controls.Add(Me.GenerateButton)
         Me.Name = "DiceRollForm"
         Me.Text = "DiceRollForm"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents GenerateButton As Button
+    Friend WithEvents DisplayListBox As ListBox
+    Friend WithEvents QuitButton As Button
+    Friend WithEvents SelectedIndexLabel As Label
 End Class
--- S5-WK3-2-DiceRoll/DiceRollForm.vb
+++ S5-WK3-2-DiceRoll/DiceRollForm.vb
@@ -9,4 +9,40 @@ Option Explicit On
 
 Public Class DiceRollForm
 
+    'Stores the dice roll results in a two dimensional array with 10,000 possible rolls
+    Dim resultArray(1, 10000) As Integer
+    'Creates an instance of the random class and the new generates a new seed each run through to provide pseudo randomness
+    Dim random As New Random()
+
+    'When the generate button is pressed
+    Private Sub GenerateButton_Click(sender As Object, e As EventArgs) Handles GenerateButton.Click
+
+        'Generates two random integers from 1 to 6 and stores them into the result array
+        For n As Integer = 0 To CInt((resultArray.Length() / 2)) - 1
+            'Generates a random integer from 1 to 6 inclusively and stores them into the result array
+            resultArray(0, n) = random.Next(1, 7)
+            resultArray(1, n) = random.Next(1, 7)
+        Next
+
+        'Iterates through the resultArray after clearing it and pushes the data into the display list box
+        DisplayListBox.Items.Clear()
+        For n As Integer = 0 To CInt((resultArray.Length() / 2)) - 1
+            'This will add the two dice rolls and push the result to the list box
+            DisplayListBox.Items.Add(resultArray(0, n) + resultArray(1, n))
+        Next
+
+    End Sub
+
+    'When the quit button is pressed
+    Private Sub QuitButton_Click(sender As Object, e As EventArgs) Handles QuitButton.Click
+        Me.Close()
+    End Sub
+
+    'When the user selects a number in the list box, fill the display list box index label
+    Private Sub DisplayListBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles DisplayListBox.SelectedIndexChanged
+        SelectedIndexLabel.Text = "You Selected Roll: " & vbCrLf & DisplayListBox.SelectedIndex.ToString
+        SelectedIndexLabel.Text = SelectedIndexLabel.Text & vbCrLf & "Roll 1: " & resultArray(0, DisplayListBox.SelectedIndex)
+        SelectedIndexLabel.Text = SelectedIndexLabel.Text & vbCrLf & "Roll 2: " & resultArray(1, DisplayListBox.SelectedIndex)
+    End Sub
+
 End Class
