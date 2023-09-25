8ee6a50bc6fe5cc33e2c2ea4b39821c4221ed9ac
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-09-24 21:23:08 -0600

    Add project files.

---
+++ S5-WK3-2-DiceRoll/DiceRollForm.vb
@@ -0,0 +1,12 @@
+﻿'Joshua Makuch
+'RCET 3371
+'Fall 2023
+'Dice Roll
+'https://github.com/JoshuaMakuch/S5-WK3-2-DiceRoll
+
+Option Strict On
+Option Explicit On
+
+Public Class DiceRollForm
+
+End Class
